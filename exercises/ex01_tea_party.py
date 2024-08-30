f90ca3da32525e5db7bf4a4402d8781cdab5d507
Yangruoya Wang <ruoya@ad.unc.edu>
2024-08-30 18:09:37 -0400

    new change

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -6,13 +6,13 @@ __author__: str = "730526115"
 def main_planner(guests: int) -> None:
     """A 'main planner' function that calls each and produces printed output."""
     print("A Cozy Tea Party for " + str(guests) + " People!")
-    print(
-        "Tea bags: " + str(tea_bags(guests))
-    )  # Changing people to guests so they would be in the same format.
-    print("Treats: " + str(treats(guests)))  # Changing people to guests.
-    print(
-        "Cost: $" + str(cost(tea_bags(guests), treats(guests)))
-    )  # Changing people to guests.
+    print("Tea bags: " + str(tea_bags(guests)))
+    # No need to change people to guests as it is function and the parameter name
+    # will not affect.
+    print("Treats: " + str(treats(guests)))
+    print("Cost: $" + str(cost(tea_bags(guests), treats(guests))))
+    # I used "," instead of "+" for print for the first time and found there is a
+    # blank between $ and money.
 
 
 def tea_bags(people: int) -> int:
@@ -21,12 +21,12 @@ def tea_bags(people: int) -> int:
 
 
 # Since everyone at the tea party will drink two cups of tea,
-# we should plan to have two tea bags for them.
+# we should prepare two tea bags for each of them.
 
 
 def treats(people: int) -> int:
     """Function for number of treats."""
-    return int(tea_bags(people=people) * 1.5)  # Calculate the number of treats needed.
+    return int(tea_bags(people=people) * 1.5)  # Using key argument!
 
 
 # This function assumes for each tea a guest drinks,
@@ -38,7 +38,7 @@ def cost(tea_count: int, treats_count: int) -> float:
     return tea_count * 0.50 + treats_count * 0.75
 
 
-# This means each tea bag costs $0.50 and each treat costs $0.75.
+# This means each tea bag costs $0.50, and each treat costs $0.75.
 
 if __name__ == "__main__":
     main_planner(guests=int(input("How many guests are attending your tea party? ")))
