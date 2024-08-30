a73e94f85226cac925adaec4892b1dc9c3f1cf17
Yangruoya Wang <ruoya@ad.unc.edu>
2024-08-30 17:55:16 -0400

    changes

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -1,18 +1,17 @@
 """EX01 - Tea Party Planner"""
 
-__author__ = "730526115"
+__author__: str = "730526115"
 
 
 def main_planner(guests: int) -> None:
     """A 'main planner' function that calls each and produces printed output."""
-    print("A Cozy Tea Party for", guests, "People!")
+    print("A Cozy Tea Party for " + str(guests) + " People!")
     print(
-        "Tea bags:", tea_bags(people=guests)
+        "Tea bags: " + str(tea_bags(guests))
     )  # Changing people to guests so they would be in the same format.
-    print("Treats:", treats(people=guests))  # Changing people to guests.
+    print("Treats: " + str(treats(guests)))  # Changing people to guests.
     print(
-        "Cost: $",  # Q: How to delete the 'blank' between $ and the cost value?
-        cost(tea_count=tea_bags(people=guests), treats_count=treats(people=guests)),
+        "Cost: $" + str(cost(tea_bags(guests), treats(guests)))
     )  # Changing people to guests.
 
 
@@ -27,10 +26,7 @@ def tea_bags(people: int) -> int:
 
 def treats(people: int) -> int:
     """Function for number of treats."""
-    tea_count = tea_bags(
-        people=people
-    )  # * cannot be used for function and float, so have to change the format.
-    return int(tea_count * 1.5)  # Calculate the number of treats needed.
+    return int(tea_bags(people) * 1.5)  # Calculate the number of treats needed.
 
 
 # This function assumes for each tea a guest drinks,
--- exercises/practice ls4.py
+++ exercises/practice ls4.py
@@ -6,3 +6,11 @@ def sum(num1: int, num2: int) -> int:
 def happy(word: str) -> str:
     """pretend to be happy"""
     return "No, you are not " + word + ", you are happy!"
+
+
+def sum(num1: int, num2: int) -> int:
+    """Add two numbers together"""
+    return num1 + num2
+
+
+print(sum(num1=4, num2=5))
